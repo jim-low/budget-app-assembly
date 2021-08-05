af954db07ba2d9e26dd48eda24262df0adce1ceb
jim-low <robil12380@gmail.com>
2021-08-05 17:50:02 +0800

    chore: add base files

---
+++ login.asm
@@ -0,0 +1,12 @@
+.model small
+.stack 100
+.data
+    ; put your variables here
+
+.code
+main proc
+    ; your main code here
+    ; all the best - Jim
+
+main endp
+end main
---
+++ user-interface.asm
@@ -0,0 +1,11 @@
+.model small
+.stack 100
+.data
+    ; put your variables here
+
+.code
+main proc
+    ; your main code here
+
+main endp
+end main
