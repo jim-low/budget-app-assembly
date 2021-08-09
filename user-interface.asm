88996e950eea352b4875969a9b41016c7a691901
jim-low <robil12380@gmail.com>
2021-08-09 20:24:52 +0800

    feat: add banner logo

--- user-interface.asm
+++ user-interface.asm
@@ -5,8 +5,14 @@
     expenses db "Groceries"
              db "Vehicle"
              db "Accomodation"
-             db "Bills" ; can be further expanded
+             db "Bills" ; can be further expanded if needed
              db "Insurance"
+    banner db 10, 13, " _            _          _"
+           db 10, 13, "| |__ _  _ __| |__ _ ___| |_   __ _ _ __ _ __"
+           db 10, 13, "| '_ \ || / _` / _` / -_)  _| / _` | '_ \ '_ \"
+           db 10, 13, "|_.__/\_,_\__,_\__, \___|\__| \__,_| .__/ .__/"
+           db 10, 13, "               |___/               |_|  |_|"
+           db "$"
     user_choice db 0
 
 .code
