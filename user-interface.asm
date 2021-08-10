8eacac7d316d279f9f5bc2054fd34b9ae44522e5
jim-low <robil12380@gmail.com>
2021-08-10 13:10:41 +0800

    feat: add main menu, convert expenses to list

--- user-interface.asm
+++ user-interface.asm
@@ -1,19 +1,27 @@
 .model small
 .stack 100
 .data
-    ; choices go by ascending order (1..5)
-    expenses db "Groceries"
-             db "Vehicle"
-             db "Accomodation"
-             db "Bills" ; can be further expanded if needed
-             db "Insurance"
     banner db 10, 13, " _            _          _"
            db 10, 13, "| |__ _  _ __| |__ _ ___| |_   __ _ _ __ _ __"
            db 10, 13, "| '_ \ || / _` / _` / -_)  _| / _` | '_ \ '_ \"
            db 10, 13, "|_.__/\_,_\__,_\__, \___|\__| \__,_| .__/ .__/"
            db 10, 13, "               |___/               |_|  |_|"
            db "$"
-    user_choice db 0
+
+    main_menu db "============== MAIN MENU =============="
+       db 10, 13 "1. Record Transaction"
+       db 10, 13 "2. Display Overall Budget Usage"
+       db 10, 13 "3. Display Total Income Percentage"
+       db 10, 13 "4. Display Total Expenses Percentage"
+       db 10, 13 "======================================="
+       db "$"
+
+    expenses_list db "1. Groceries"
+          db 10, 13, "2. Vehicle"
+          db 10, 13, "3. Accomodation"
+          db 10, 13, "4. Bills" ; can be further expanded if needed
+          db 10, 13, "5. Insurance"
+          db "$"
 
 .code
 main proc
