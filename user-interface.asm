a2ac93fe95c40ecd2364f0836c0b27a0a88b0fa0
jim-low <robil12380@gmail.com>
2021-08-23 18:59:10 +0800

    feat: add banner and successful message for sign up

--- user-interface.asm
+++ user-interface.asm
@@ -23,22 +23,19 @@
                   db 10, 13, "                      ============================="
                   db "$"
 
+    signup_banner db 10, 13, "   ____            _     _                 _                             _"
+                  db 10, 13, "  |  _ \ ___  __ _(_)___| |_ ___ _ __     / \   ___ ___ ___  _   _ _ __ | |_"
+                  db 10, 13, "  | |_) / _ \/ _` | / __| __/ _ \ '__|   / _ \ / __/ __/ _ \| | | | '_ \| __|"
+                  db 10, 13, "  |  _ <  __/ (_| | \__ \ ||  __/ |     / ___ \ (_| (_| (_) | |_| | | | | |_"
+                  db 10, 13, "  |_| \_\___|\__, |_|___/\__\___|_|    /_/   \_\___\___\___/ \__,_|_| |_|\__|"
+                  db 10, 13, "             |___/"
+                  db "$"
+
+    signup_successful_msg db 10, 13, "                        -- Successfully Signed Up! --$"
+
     include DATETIME.INC
 
-; TODOS:
-; - Menus to display:
-;    - Expenses
-; - display current budget
-; - check with team if there are other menus
-
 .code
-show_datetime proc
-    call show_time
-    call show_date
-
-    ret
-show_datetime endp
-
 show_main_menu proc
     lea dx, banner
     mov ah, 09h
@@ -52,6 +49,18 @@ show_main_menu proc
 
     ret
 show_main_menu endp
+
+show_signup proc
+    lea dx, signup_banner
+    mov ah, 09h
+    int 21h
+    ret
+show_signup endp
+
+show_successful_signup proc
+    CHANGE_COLOR 02h , signup_successful_msg
+    ret
+show_successful_signup endp
 
 list_expenses proc
     lea dx, expenses_menu
