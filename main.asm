f8e7ce497db8266c6fc4f6f01c98203387984e24
jim-low <robil12380@gmail.com>
2021-08-28 21:10:56 +0800

    chore: fix naming convention

--- main.asm
+++ main.asm
@@ -1,8 +1,8 @@
 .model small
 .stack 100
 .data
-    depart_msg db "thank you for the headache-inducing assignment and the painful torture of learning the assembly language"
-               db 13, 10, "we are not grateful and forever resentful. :d$"
+    departMsg db "Thank you for the headache-inducing assignment and the painful torture of learning the Assembly Language"
+               db 13, 10, "We are not grateful and forever resentful. :D$"
     include utils.inc
     include sign-up.asm
     include login.asm
@@ -14,7 +14,7 @@ main proc
     mov ax, @data
     mov ds, ax
 
-endprogram:
+EndProgram:
     mov ah, 4ch
     int 21h
 
