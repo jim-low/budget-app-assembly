6377da21085fcd3aca0f3e14b7970913c8434b29
diongzy <yuii.yuii265@gmail.com>
2021-09-02 18:09:03 +0800

    feat: update formula

--- formulas.asm
+++ formulas.asm
@@ -1,242 +1,244 @@
 .data
-    prompt_choice DB 0DH, 0AH, "Enter your choice(1 - groceries, 2 - bills): $"
-    prompt_groceries DB 0DH, 0AH, "Enter your groceries expenses: $"
-	prompt_bills DB 0DH, 0AH, "Enter your bills expenses: $"
-	WARNING DB "DOSBOX does not support 32 bits and above$"
-	CHOICE DB ?
-    TEN DW 10
-	HUNDRED DW 100
-	expenses_array DW 0, 0, 0, 0, 0
-	groceries_SST DW ?
-	groceries_buffer DW 18, ?, 20 dup ("$")
-	groceries_tail DW 0  ;tail is different and fixed for every expense
-	groceries_expenses DW 1234
-	bills_buffer DW 18, ?, 20 dup ("$")
-	bills_tail DW 1      ;tail is different and fixed for every expense
-	bills_expenses DW 0
-	SumOfAllExpenses DW 0
-	NewBalance DW 0
-	CurrentBalance DW 9855
-	OverallBudgetUsage DW ?
-	temp DW 0
-	printable_num DW 10 DUP ("$")
-	NL DB 0DH, 0AH, "$"
+    promptChoice db 0dh, 0ah, "enter your choice(1 - groceries, 2 - bills): $"
+    promptGroceries db 0dh, 0ah, "enter your groceries expenses: $"
+	promptVehicle db 0dh, 0ah, "enter your vehicle expenses: $"
+	promptAccomodation db 0dh, 0ah, "enter your accomodation expenses: $"
+	promptBills db 0dh, 0ah, "enter your bills expenses: $"
+    promptInsurance db 0dh, 0ah, "enter your insurance expenses: $"
+	promptIncome db 0dh, 0ah, "enter your income: $"
+	incomeBuffer dw 18, ?, 20 dup ("$")
+	currentIncome dw 0
+    warning db "dosbox does not support 32 bits and above$"
+    choice db ?
+    five dw 5
+    ten dw 10
+    hundred dw 100
+    expensesArray dw 0, 0, 0, 0, 0
+    groceriesSST dw ?
+    groceriesBuffer dw 18, ?, 20 dup ("$")
+    groceriesTail dw 0
+    groceriesExpenses dw 0
+	vehicleBuffer dw 18, ?, 20 dup ("$")
+    vehicleTail dw 1
+    vehicleExpenses dw 0
+	accomodationBuffer dw 18, ?, 20 dup ("$")
+    accomodationTail dw 2
+    accomodationExpenses dw 0
+    billsBuffer dw 18, ?, 20 dup ("$")
+    billsTail dw 1
+    billsExpenses dw 0
+	insuranceBuffer dw 18, ?, 20 dup ("$")
+    insuranceTail dw 4
+    insuranceExpenses dw 0
+    sumOfAllExpenses dw 0
+    newBalance dw 0
+    currentBalance dw 0
+    overallBudgetUsage dw ?
 
 .code
-    ; all the best - Jim
-	    MOV AX, @DATA
-		MOV DS, AX
-START:
-		MOV AH, 09H
-		LEA DX, prompt_choice
-		INT 21H
-		
-		MOV AH, 01H
-		INT 21H
-		SUB AL, 30H
-		MOV CHOICE, AL
-		
-		CMP CHOICE, 1
-		JE PROMPT_GROCERIES_EXPENSES
-		CMP CHOICE, 2
-		JE PROMPT_BILLS_EXPENSES
-		
-;BudgetUsage = Sum Of All Expenses In That Area 
-;(ALL EXPENSES IN ONE ARRAY)(DONE, use this)		
-PROMPT_GROCERIES_EXPENSES:
 
-		MOV AH, 09H
-		LEA DX, prompt_groceries
-		INT 21H
-		
-		MOV AH, 0AH
-		LEA DX, groceries_buffer
-		INT 21H
-		
-		MOV AH, 09H
-		LEA DX, NL
-		INT 21H
-		
-		LEA SI, groceries_buffer
-		LEA DI, groceries_expenses   ;to store the current groceries expense, which will be added to expenses_array later
-		CALL ConvertToNum
-		CALL CalculateGroceriesSST
-		
-		LEA SI, expenses_array
-		MOV AX, groceries_expenses  ;groceries_expenses = 1234
-		MOV BX, groceries_tail      ;groceries_tail = 0
-		CALL InsertIntoExpensesArray
-		
-		MOV AH, 09H
-		LEA DX, NL
-		INT 21H
-		CALL START
+promptForIncome:
+    lea di, incomeBuffer
+    lea si, promptIncome
+    mov singleInput, 0
+    call Prompt
 
-PROMPT_BILLS_EXPENSES:
+    NEW_LINE
 
-		MOV AH, 09H
-		LEA DX, prompt_bills
-		INT 21H
-		
-		MOV AH, 0AH
-		LEA DX, bills_buffer
-		INT 21H
-		
-		MOV AH, 09H
-		LEA DX, NL
-		INT 21H
-		
-		LEA SI, bills_buffer
-		LEA DI, bills_expenses
-		CALL ConvertToNum
-		
-		LEA SI, expenses_array  ;load the address of the array into SI, SI is now pointing to the array
-		MOV AX, bills_expenses
-		MOV BX, bills_tail      ;BX = 1
-		CALL InsertIntoExpensesArray
-		
-		MOV AH, 09H
-		LEA DX, NL
-		INT 21H
-		
-		
-		
-		
-;NewBalance = CurrentBalance - Expenses (DONE)
-SUM_ALL_EXPENSES:
-	MOV CX, 5
-	MOV AX, 0
-	LEA SI, expenses_array
-SUM_UP:
-	MOV AX, [SI]
-	ADD SumOfAllExpenses, AX
-	ADD SI, 2
-	LOOP SUM_UP
+    lea si, incomeBuffer
+    lea di, currentIncome
+    call ConvertToNum
+
+Start:
+    lea di, choice
+    lea si, promptChoice
+    mov singleInput, 1
+    call Prompt
+
+    cmp choice, 1
+    je promptGroceriesExpenses
+    cmp choice, 2
+    je promptBillsExpenses
+
+;budgetusage = sum of all expenses in that area
+;(all expenses in one array)(done, use this)
+promptGroceriesExpenses:
+    lea di, groceriesBuffer
+    lea si, promptGroceries
+    mov singleInput, 0
+    call Prompt
+
+    NEW_LINE
+
+    lea si, groceriesBuffer
+    lea di, groceriesExpenses
+    call ConvertToNum
+    call CalculateGroceriesSST
+
+    lea si, expensesArray
+    mov ax, groceriesExpenses
+    mov bx, groceriesTail
+    call InsertIntoExpensesArray
+
+    CLEAR
+    call Start
+
+promptVehicleExpenses:
+    lea di, vehicleBuffer
+    lea si, promptVehicle
+    mov singleInput, 0
+    call Prompt
+
+    NEW_LINE
+
+    lea si, vehicleBuffer
+    lea di, vehicleExpenses
+    call ConvertToNum
+
+    lea si, expensesArray
+    mov ax, vehicleExpenses
+    mov bx, vehicleTail
+    call InsertIntoExpensesArray
 	
-NEW_BALANCE:
-	MOV AX, CurrentBalance
-	SUB AX, SumOfAllExpenses
-	MOV NewBalance, AX
+	CLEAR
+    call Start
 
+promptAccomodationExpenses:
+    lea di, accomodationBuffer
+    lea si, promptAccomodation
+    mov singleInput, 0
+    call Prompt
 
-;OverallBudgetUsage = Sum Of All Expenses * 100 / PreviousBalance (Forever WIP)
-	MOV DX, 0
-	MOV AX, SumOfAllExpenses
-	CMP AX, 655
-	JA WARNING_MSG
-	MUL HUNDRED
-	DIV CurrentBalance
-	MOV OverallBudgetUsage, AX
-		
-	LEA SI, printable_num
-	LEA DI, OverallBudgetUsage    ;digit that needs to be printed
-	MOV BX, 0
-	CALL ConvertToChar
-	MOV temp, BX
-	MOV CX, temp
-	LEA SI, printable_num
-	ADD SI, temp
-	CALL PrintMultipleDigitsNum
-		
-	;JMP END_PROGRAM  ;no END_PROGRAM here, so comment it first
-		
-WARNING_MSG:
-    MOV AH, 09H
-    LEA DX, WARNING
-	INT 21H
-		
-		
-		
-		
+    NEW_LINE
 
-;FUNCTIONS
-ConvertToChar proc near
+    lea si, accomodationBuffer
+    lea di, accomodationExpenses
+    call ConvertToNum
 
-CONVERT_TO_CHAR:
-        MOV CX, 0
-		MOV DX, 0
-		MOV DX, [DI]
-        CMP DX, 0
-		JE END_CHAR_CONVERSION
-		MOV DX, 0
-		MOV AX, [DI]
-		DIV TEN
-		MOV [DI], AX
-		MOV AX, 0
-		ADD DX, 30H
-		MOV [SI], DL
-		INC CX
-		ADD BX, CX
-		INC SI
-		JMP CONVERT_TO_CHAR
-		
-END_CHAR_CONVERSION:
-        RET
-		
-ConvertToChar endp
+    lea si, expensesArray
+    mov ax, accomodationExpenses
+    mov bx, accomodationTail
+    call InsertIntoExpensesArray
+	
+	CLEAR
+    call Start
 
-PrintMultipleDigitsNum proc near
+promptBillsExpenses:
+    lea di, billsBuffer
+    lea si, promptBills
+    mov singleInput, 0
+    call Prompt
 
-PRINTING: 
-        MOV AH, 02H
-		MOV DL, [SI-1];3 2 1 0
-		INT 21H
-		DEC SI
-		LOOP PRINTING
-		RET
+    CLEAR
 
-PrintMultipleDigitsNum endp
+    lea si, billsBuffer
+    lea di, billsExpenses
+    call ConvertToNum
+
+    lea si, expensesArray  ;load the address of the array into si, si is now pointing to the array
+    mov ax, billsExpenses
+    mov bx, billsTail
+    call InsertIntoExpensesArray
+
+    CLEAR
+    call Start
+	
+promptInsuranceExpenses:
+    lea di, insuranceBuffer
+    lea si, promptInsurance
+    mov singleInput, 0
+    call Prompt
+
+    NEW_LINE
+
+    lea si, insuranceBuffer
+    lea di, insuranceExpenses
+    call ConvertToNum
+
+    lea si, expensesArray
+    mov ax, insuranceExpenses
+    mov bx, insuranceTail
+    call InsertIntoExpensesArray
+	
+	CLEAR
+    call Start
+
+    ;newbalance = currentbalance - expenses (done)
+SumAllExpenses:
+    mov cx, 5
+    mov ax, 0
+    lea si, expensesArray
+
+SumUp:
+    mov ax, [si]
+    add sumOfAllExpenses, ax
+    add si, 2
+    loop SumUp
+
+CalculateNewBalance:
+    mov ax, currentBalance
+    sub ax, sumOfAllExpenses
+    mov newBalance, ax
+
+    ;overallbudgetusage = sum of all expenses * 100 / previousbalance (forever wip)
+    mov dx, 0
+    mov ax, sumOfAllExpenses
+    cmp ax, 655
+    ja warningMsg
+    mul hundred
+    div currentBalance
+    mov overallBudgetUsage, ax
+
+    ;jmp end_program  ;no end_program here, so comment it first
+
+warningMsg:
+    CHANGE_COLOR 04h, warning
 
 ConvertToNum proc near
-		MOV BX, 2
-		MOV AX, 0
-CONVERT_TO_NUM:
-        MOV Dl, [SI]
-		CMP Dl, 0Dh
-		JE END_CONVERSION
-		
-		MOV DX, 0
-		MOV AX, [DI]
-		MUL TEN
-		ADD SI, BX  ;[SI+BX]
-		ADD DX, [SI]
-		MOV DH, 0
-		SUB DX, 30H
-		ADD AX, DX
-		MOV [DI], AX
-		INC SI
-		MOV BX, 0
-		JMP CONVERT_TO_NUM
-		
-END_CONVERSION:
-        RET
-		
+    mov bx, 2
+    mov ax, 0
+Convert:
+    mov dl, [si]
+    cmp dl, 0dh
+    je EndConversion
+
+    mov dx, 0
+    mov ax, [di]
+    mul ten
+    add si, bx  ;[si+bx]
+    add dx, [si]
+    mov dh, 0
+    sub dx, 30h
+    add ax, dx
+    mov [di], ax
+    inc si
+    mov bx, 0
+    jmp Convert
+
+EndConversion:
+    ret
 ConvertToNum endp
 
 CalculateGroceriesSST proc near
+    mov ax, groceriesExpenses
+    mul five
+    div hundred
+    mov groceriesSST, ax
+    mov ax, groceriesExpenses
+    add ax, groceriesSST
+    mov groceriesExpenses, ax
 
-    MOV AX, groceries_expenses
-	MUL FIVE
-	DIV HUNDRED
-	MOV groceries_SST, AX
-	MOV AX, groceries_expenses
-	ADD AX, groceries_SST
-	MOV groceries_expenses, AX
-	RET
-	
+    ret
 CalculateGroceriesSST endp
 
 InsertIntoExpensesArray proc near
+InsertIntoExpensesArr:
+    ;add si, bx    ;[si+bx]
+    add [si+bx], ax  ;access the memory of si => location of the 1st element => add the value of ax there
+    mov bx, 0
+    jmp EndInsertion
 
-INSERT_INTO_EXPENSES_ARR:
-        ;ADD SI, BX    ;[SI+BX]
-        ADD [SI+BX], AX  ;access the memory of SI => location of the 1st element => add the value of AX there
-		MOV BX, 0
-		JMP END_INSERTION
-
-END_INSERTION:
-        RET
-		
+EndInsertion:
+    ret
 InsertIntoExpensesArray endp
-
 
