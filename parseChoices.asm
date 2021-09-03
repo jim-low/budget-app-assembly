f08745a8f9f638b2ebc35f1e45fbd5feadb98c84
jim-low <robil12380@gmail.com>
2021-09-03 12:20:00 +0800

    feat: add main menu parse for choice 1

--- parseChoices.asm
+++ parseChoices.asm
@@ -4,4 +4,15 @@
 ; 3. expenses choices
 
 .code
+ParseMainMenu proc
+    jmp StartParseMainMenu
+
+SelectRecordTransaction:
+    call RecordTransaction
+
+StartParseMainMenu:
+    cmp choice, "1"
+    je SelectRecordTransaction
+
+ParseMainMenu endp
 
