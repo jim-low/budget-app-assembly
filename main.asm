9ef36584102ebe231ba5654943f5e0276e6163e7
jim-low <robil12380@gmail.com>
2021-09-06 04:07:20 +0800

    fix: remove choice error and departure msg variables

--- main.asm
+++ main.asm
@@ -1,11 +1,9 @@
 .model small
 .stack 100
 .data
-    departMsg db "Thank you for the headache-inducing assignment and the painful torture of learning the Assembly Language"
-              db 13, 10, "We are not grateful and forever resentful. :D$"
-    choiceErrorMsg db "clearly the list is only from 1 to 6, but you still went over it you donkey$"
     choicePrompt db "                             Enter your choice: $"
     choice db ?
+
     totalExpensesPercentageMsg db "Your total expenses percentage is 69420% lmao, you broke af$"
     totalIncomePercentageMsg db "Your total income percentage is 0% lmao$"
 
@@ -83,9 +81,7 @@ ProgramStart:
     call ParseMainMenu
 
 EndProgram:
-    lea si, departMsg
-    mov dl, stringFlag
-    call Display
+    call ShowExitScreen
 
     mov ah, 4ch
     int 21h
